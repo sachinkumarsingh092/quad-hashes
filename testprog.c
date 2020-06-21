85855fed005b63ac21ee738b9b4c9e5dcbef2158
Sachin Kumar Singh <sachinkumarsingh092@gmail.com>
2020-06-21 19:19:37 +0530

    Added python scripts to make HEALPix plots

--- testprog.c
+++ testprog.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <chealpix.h>
+
 #include <gnuastro/table.h>
 #include <gnuastro/statistics.h>
+
 
 int main(){
 
@@ -14,11 +17,12 @@ int main(){
     gal_list_str_reverse(&cols);
 
     /* Read the columns. */
-    gal_data_t *ref=gal_table_read ( "gaia-dr2-near-castor.fits", "1", NULL, cols, GAL_TABLE_SEARCH_NAME, 
+    gal_data_t *ref=gal_table_read("gaia-dr2-near-castor.fits", "1",
+                                    NULL, cols, GAL_TABLE_SEARCH_NAME,
                                     0, -1, 0, NULL);
 
     /* Seperate columns. */
-    gal_data_t *ra=gal_data_copy_to_new_type (ref, GAL_TYPE_FLOAT64);
+    gal_data_t *ra= gal_data_copy_to_new_type (ref, GAL_TYPE_FLOAT64);
     gal_data_t *dec=gal_data_copy_to_new_type (ref->next, GAL_TYPE_FLOAT64);
     gal_data_t *mag=gal_data_copy_to_new_type (ref->next->next, GAL_TYPE_FLOAT32);
 
@@ -29,7 +33,6 @@ int main(){
     gal_data_t *ra_min=gal_statistics_minimum (ra);
     gal_data_t *ra_max=gal_statistics_maximum (ra);
 
-
     /* Make an array of the columns. */
     double *c1=ra->array;
     double *c2=dec->array;
@@ -37,6 +40,9 @@ int main(){
 
     double *min_c1=ra_min->array;
     double *max_c1=ra_max->array;
+
+    /* Make a healpix of the magnitude data. */
+    write_healpix_map(c3, 2, "healpix-test.fits", 0, "C");
 
     printf("%lf %lf\n", min_c1[0], max_c1[0]);
 
